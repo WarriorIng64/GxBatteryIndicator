fa788e8a1e5a65ea80b8c8cf681b977416b8af72
Christopher Kyle Horton <christhehorton@gmail.com>
2014-03-31 02:13:05 -0400

    Work around weird scope error.

--- GxBatteryIndicator.py
+++ GxBatteryIndicator.py
@@ -29,27 +29,28 @@ self.battery_charging = pygame.image.load("indicators/default/GxBatteryIndicator
 self.SetIcon(pygame.image.load("indicators/default/GxBatteryIndicator/battery_base.png"))
 
 self.frame_code = """
+global ctypes
 import platform
+import ctypes
+from ctypes import wintypes
 
 if platform.system() == 'Windows':
-  import ctypes
-  from ctypes import wintypes
 
   class SYSTEM_POWER_STATUS(ctypes.Structure):
     _fields_ = [
-      ('ACLineStatus', wintypes.BYTE),
-      ('BatteryFlag', wintypes.BYTE),
-      ('BatteryLifePercent', wintypes.BYTE),
-      ('Reserved1', wintypes.BYTE),
-      ('BatteryLifeTime', wintypes.DWORD),
-      ('BatteryFullLifeTime', wintypes.DWORD),
+      ('ACLineStatus', ctypes.wintypes.BYTE),
+      ('BatteryFlag', ctypes.wintypes.BYTE),
+      ('BatteryLifePercent', ctypes.wintypes.BYTE),
+      ('Reserved1', ctypes.wintypes.BYTE),
+      ('BatteryLifeTime', ctypes.wintypes.DWORD),
+      ('BatteryFullLifeTime', ctypes.wintypes.DWORD),
     ]
 
   SYSTEM_POWER_STATUS_P = ctypes.POINTER(SYSTEM_POWER_STATUS)
 
   GetSystemPowerStatus = ctypes.windll.kernel32.GetSystemPowerStatus
   GetSystemPowerStatus.argtypes = [SYSTEM_POWER_STATUS_P]
-  GetSystemPowerStatus.restype = wintypes.BOOL
+  GetSystemPowerStatus.restype = ctypes.wintypes.BOOL
 
   status = SYSTEM_POWER_STATUS()
   if not GetSystemPowerStatus(ctypes.pointer(status)):
