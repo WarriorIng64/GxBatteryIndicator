e55810fd01ba03fd6aaa7048f69b1f05bb0faf72
Christopher Kyle Horton <christhehorton@gmail.com>
2014-04-05 01:01:03 -0400

    Only check power about once a second.

--- GxBatteryIndicator.py
+++ GxBatteryIndicator.py
@@ -31,101 +31,109 @@ self.battery_unknown = pygame.image.load("indicators/default/GxBatteryIndicator/
 
 self.SetIcon(self.battery_unknown)
 
+self.current_frame = 0
+self.frames_to_wait = 60
+
 self.frame_code = """
-import platform
-
-self.image = self.icon
-self.status_string = ""
-
-if platform.system() == 'Windows':
+self.current_frame += 1
+if self.current_frame >= self.frames_to_wait:
   
-  global ctypes
-  import ctypes
-  from ctypes import wintypes
-
-  class SYSTEM_POWER_STATUS(ctypes.Structure):
-    _fields_ = [
-      ('ACLineStatus', ctypes.wintypes.BYTE),
-      ('BatteryFlag', ctypes.wintypes.BYTE),
-      ('BatteryLifePercent', ctypes.wintypes.BYTE),
-      ('Reserved1', ctypes.wintypes.BYTE),
-      ('BatteryLifeTime', ctypes.wintypes.DWORD),
-      ('BatteryFullLifeTime', ctypes.wintypes.DWORD),
-    ]
-
-  SYSTEM_POWER_STATUS_P = ctypes.POINTER(SYSTEM_POWER_STATUS)
-
-  GetSystemPowerStatus = ctypes.windll.kernel32.GetSystemPowerStatus
-  GetSystemPowerStatus.argtypes = [SYSTEM_POWER_STATUS_P]
-  GetSystemPowerStatus.restype = ctypes.wintypes.BOOL
-
-  status = SYSTEM_POWER_STATUS()
-  if not GetSystemPowerStatus(ctypes.pointer(status)):
-    raise ctypes.WinError()
-
-  if status.ACLineStatus == 1:
-    self.image = self.battery_charging
-  elif status.BatteryLifePercent > 90:
-    self.image = self.battery_100
-  elif status.BatteryLifePercent > 70:
-    self.image = self.battery_80
-  elif status.BatteryLifePercent > 50:
-    self.image = self.battery_60
-  elif status.BatteryLifePercent > 30:
-    self.image = self.battery_40
-  elif status.BatteryLifePercent > 10:
-    self.image = self.battery_20
-  else:
-    self.image = self.battery_critical
+  import platform
   
-  self.status_string = "Your battery is " + str(status.BatteryLifePercent) + "% charged"
-  if status.ACLineStatus == 1:
-    self.status_string += " and is plugged in"
-  self.status_string += "."
+  self.image = self.icon
+  self.status_string = ""
   
-elif platform.system() == 'Linux':
-  
-  # Ubuntu and elementary OS are supported
-  
-  import subprocess
-  
-  power_sources = []
-  
-  pipe = subprocess.Popen(["upower", "-e"], stdout=subprocess.PIPE).stdout
-  for power_source in pipe:
-    power_sources.append(power_source.strip())
-  
-  percentages = []
-  for power_source in power_sources:
-    if "battery" in power_source:
-      source_pipe = subprocess.Popen(["upower", "-i", power_source], stdout=subprocess.PIPE).stdout
-      for line in source_pipe:
-        if "percentage" in line:
-          import re
-          percentage = float((re.findall(r"\d*\.\d+|\d+", line))[0])
-          percentages.append(percentage)
-  if len(percentages) > 0:
-    average_percentage = sum(percentages) / float(len(percentages))
-    if average_percentage > 90:
+  if platform.system() == 'Windows':
+    
+    global ctypes
+    import ctypes
+    from ctypes import wintypes
+    
+    class SYSTEM_POWER_STATUS(ctypes.Structure):
+      _fields_ = [
+        ('ACLineStatus', ctypes.wintypes.BYTE),
+        ('BatteryFlag', ctypes.wintypes.BYTE),
+        ('BatteryLifePercent', ctypes.wintypes.BYTE),
+        ('Reserved1', ctypes.wintypes.BYTE),
+        ('BatteryLifeTime', ctypes.wintypes.DWORD),
+        ('BatteryFullLifeTime', ctypes.wintypes.DWORD),
+      ]
+    
+    SYSTEM_POWER_STATUS_P = ctypes.POINTER(SYSTEM_POWER_STATUS)
+    
+    GetSystemPowerStatus = ctypes.windll.kernel32.GetSystemPowerStatus
+    GetSystemPowerStatus.argtypes = [SYSTEM_POWER_STATUS_P]
+    GetSystemPowerStatus.restype = ctypes.wintypes.BOOL
+    
+    status = SYSTEM_POWER_STATUS()
+    if not GetSystemPowerStatus(ctypes.pointer(status)):
+      raise ctypes.WinError()
+    
+    if status.ACLineStatus == 1:
+      self.image = self.battery_charging
+    elif status.BatteryLifePercent > 90:
       self.image = self.battery_100
-    elif average_percentage > 70:
+    elif status.BatteryLifePercent > 70:
       self.image = self.battery_80
-    elif average_percentage > 50:
+    elif status.BatteryLifePercent > 50:
       self.image = self.battery_60
-    elif average_percentage > 30:
+    elif status.BatteryLifePercent > 30:
       self.image = self.battery_40
-    elif average_percentage > 10:
+    elif status.BatteryLifePercent > 10:
       self.image = self.battery_20
     else:
       self.image = self.battery_critical
-    self.status_string = "Your laptop is " + str(average_percentage) + "% charged"
+    
+    self.status_string = "Your battery is " + str(status.BatteryLifePercent) + "% charged"
+    if status.ACLineStatus == 1:
+      self.status_string += " and is plugged in"
+    self.status_string += "."
+    
+  elif platform.system() == 'Linux':
+    
+    # Ubuntu and elementary OS are supported
+    
+    import subprocess
+    
+    power_sources = []
+    
+    pipe = subprocess.Popen(["upower", "-e"], stdout=subprocess.PIPE).stdout
+    for power_source in pipe:
+      power_sources.append(power_source.strip())
+    
+    percentages = []
+    for power_source in power_sources:
+      if "battery" in power_source:
+        source_pipe = subprocess.Popen(["upower", "-i", power_source], stdout=subprocess.PIPE).stdout
+        for line in source_pipe:
+          if "percentage" in line:
+            import re
+            percentage = float((re.findall(r"\d*\.\d+|\d+", line))[0])
+            percentages.append(percentage)
+    if len(percentages) > 0:
+      average_percentage = sum(percentages) / float(len(percentages))
+      if average_percentage > 90:
+        self.image = self.battery_100
+      elif average_percentage > 70:
+        self.image = self.battery_80
+      elif average_percentage > 50:
+        self.image = self.battery_60
+      elif average_percentage > 30:
+        self.image = self.battery_40
+      elif average_percentage > 10:
+        self.image = self.battery_20
+      else:
+        self.image = self.battery_critical
+      self.status_string = "Your laptop is " + str(average_percentage) + "% charged"
+    else:
+      self.status_string = "No battery was found"
+    
+    self.status_string += " (" + str(len(power_sources)) + " power sources)."
+    
   else:
-    self.status_string = "No battery was found"
+    self.status_string = "Your battery status is unknown."
   
-  self.status_string += " (" + str(len(power_sources)) + " power sources)."
+  self.SetClickCode('self.wm.ShowPopupMessage("Battery Status", self.status_string)')
   
-else:
-  self.status_string = "Your battery status is unknown."
-
-self.SetClickCode('self.wm.ShowPopupMessage("Battery Status", self.status_string)')
+  self.current_frame = 0
 """
